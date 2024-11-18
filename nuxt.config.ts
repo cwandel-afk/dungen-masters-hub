a73f1fabbd1412971c1eecb450e57415e5375f08
Christopher Wandel <cwandel2@gmail.com>
2024-11-18 17:26:28 -0500

    add @nuxthub/core module and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,10 +12,13 @@ export default defineNuxtConfig({
       mdc: true,
     },
   },
-  modules: ["@nuxt/content", "@pinia/nuxt"],
+  modules: ["@nuxt/content", "@pinia/nuxt", "@nuxthub/core"],
   css: ["~/assets/css/main.css", "~/assets/css/forms-fields.css"],
   routeRules: {
     "/": { prerender: true },
   },
   compatibilityDate: "2024-11-15",
+  hub: {
+    database: true,
+  },
 });
