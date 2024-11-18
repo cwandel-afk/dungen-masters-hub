d9eda4e4a1f43e5e9f5f0a637de099b724e6d5ba
Christopher Wandel <cwandel2@gmail.com>
2024-11-18 18:39:42 -0500

    format nuxt.config.ts for improved readability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -15,7 +17,9 @@ export default defineNuxtConfig({
   modules: ["@nuxt/content", "@pinia/nuxt", "@nuxthub/core"],
   css: ["~/assets/css/main.css", "~/assets/css/forms-fields.css"],
   routeRules: {
-    "/": { prerender: true },
+    "/": {
+      prerender: true,
+    },
   },
   compatibilityDate: "2024-11-15",
   hub: {
